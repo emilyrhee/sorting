e50c306d68eb145ad25df5eaef93405a99f7b369
emilyrhee <emilyrhee99@gmail.com>
2023-09-16 13:41:38 -0400

    removed Insertion class and made into function

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!"); 
+    public static void insertionSort(int array[]) {  
+        int n = array.length;  
+        for (int j = 1; j < n; j++) {  
+            int key = array[j];  
+            int i = j-1;  
+            while ( (i > -1) && ( array [i] > key ) ) {  
+                array [i+1] = array [i];  
+                i--;  
+            }  
+            array[i+1] = key;  
+        }
     }
+
+    public static void main(String[] args) {
+        int[] arr1 = {9,14,3,2,43,11,58,22};   
+
+        insertionSort(arr1);
+
+        for(int i:arr1){    
+            System.out.print(i+" ");    
+            }        
+        }
 }
