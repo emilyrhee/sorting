178440c33f39a9b85243cfc645c39b7d8a072372
emilyrhee <emilyrhee99@gmail.com>
2023-09-17 19:39:31 -0400

    added selectionSort() function

--- Main.java
+++ Main.java
@@ -3,6 +3,28 @@ import java.util.Scanner;
 import java.io.File;
 
 public class Main {
+    public static void selectionSort(ArrayList<Integer> list) {
+        int temp, min, exchanges = 0, comparisons = 0;
+        int numberOfItems = list.size();
+        
+        for (int pass = 0; pass != numberOfItems - 1; pass++) {
+            min = pass;
+            
+            for (int index = pass + 1; index != numberOfItems; index++) {
+                comparisons++;
+                if (list.get(index) < list.get(min))
+                    min = index;
+            } 
+            
+            temp = list.get(min);
+            list.set(min, list.get(pass));
+            list.set(pass, temp);
+            exchanges++;
+        }
+        System.out.println("Comparisons: " + comparisons);
+        System.out.println("Exchanges: " + exchanges);    
+    }
+
     public static void insertionSort(ArrayList<Integer> arrayList) {
         for (int i = 1; i < arrayList.size(); i++) {
             int current = arrayList.get(i);
